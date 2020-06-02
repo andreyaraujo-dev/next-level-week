08ab242de49e0ea17d039e871b6c860fdd271e83
Jacksson Andrey <jaab.andrey@gmail.com>
2020-06-02 11:42:58 -0300

    Criação do back-end da aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+import path from 'path';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  console.log('Listando usuários');
-  response.json([
-    'Jacksson',
-    'Andrey',
-    'Araujo'
-  ]);
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
