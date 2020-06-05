dd3b526c0b1f3d7d0fc7d8077d9935aa6ee812e9
Jacksson Andrey <jaab.andrey@gmail.com>
2020-06-05 12:08:36 -0300

    Inclusão de imagens no cadastro de pontos de coleta

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -6,6 +6,8 @@ import { LeafletMouseEvent } from 'leaflet';
 
 import api from '../../services/api';
 
+import Dropzone from '../../components/Dropzone';
+
 import './styles.css';
 import logo from '../../assets/logo.svg';
 import axios from 'axios';
@@ -37,6 +39,7 @@ const CreatePoint = () => {
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
   const [seletedItems, setSeletedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const [formData, setFormData] = useState({
     name: '',
@@ -119,15 +122,19 @@ const CreatePoint = () => {
     const items = seletedItems;
     const [latitude, longitude] = selectedPosition;
 
-    const data = {
-      name, 
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude, 
-      longitude,
-      items
+    const data = new FormData();
+
+    data.append('name', name); 
+    data.append('email', email); 
+    data.append('whatsapp', whatsapp); 
+    data.append('uf', uf); 
+    data.append('city', city); 
+    data.append('latitude', String(latitude)); 
+    data.append('longitude', String(longitude)); 
+    data.append('items', items.join(','));
+
+    if (selectedFile) {
+      data.append('image', selectedFile);
     }
 
     await api.post('points', data);
@@ -150,6 +157,7 @@ const CreatePoint = () => {
 
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do<br /> ponto de coleta</h1>
+        <Dropzone onFileUploaded={setSelectedFile} />
         {/* DADOS */}
         <fieldset>
           <legend><h2>Dados</h2></legend>
